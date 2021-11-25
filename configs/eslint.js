aa844b639cb16fd5f1fee178050ee2b007a1a3a6
Oscar Lagercrantz <oscar.lagercrantz@gmail.com>
2021-11-25 19:39:52 +0100

    Stricter rules

--- configs/eslint.js
+++ configs/eslint.js
@@ -72,7 +72,7 @@ module.exports = {
       "error",
       {
         blankLine: "always",
-        prev: ["const", "let", "var", "if", "for", "while"],
+        prev: ["const", "let", "var", "if", "for", "while", "throw"],
         next: "*",
       },
     ],
