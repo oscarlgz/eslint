95fe7d83d053b4c54676f431e8894ed7dd6d0502
Oscar Lagercrantz <oscar.lagercrantz@gmail.com>
2023-08-21 14:54:11 +0200

    Fix config

--- configs/sonar.js
+++ configs/sonar.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["plugin:sonarjs/recommended"],
   rules: {
-    "sonarjs/no-duplicate-string": ["error", 10],
+    "sonarjs/no-duplicate-string": ["error", { threshold: 10 }],
     "sonarjs/cognitive-complexity": ["error", 50],
     "sonarjs/prefer-immediate-return": "off",
     "sonarjs/no-identical-functions": "off",
