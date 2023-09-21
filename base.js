2f907dc13f95ba094c7b94467ada781dbd5067fa
Oscar Lagercrantz <oscar.lagercrantz@gmail.com>
2023-09-21 09:40:25 +0200

    Add sort export

--- base.js
+++ base.js
@@ -1,10 +1,11 @@
 module.exports = {
   extends: [
     require.resolve("./configs/eslint"),
-    require.resolve("./configs/typescript-eslint"),
-    require.resolve("./configs/sonar"),
-    require.resolve("./configs/jest"),
+    require.resolve("./configs/export"),
     require.resolve("./configs/import"),
+    require.resolve("./configs/jest"),
+    require.resolve("./configs/sonar"),
+    require.resolve("./configs/typescript-eslint"),
     require.resolve("./configs/unicorn"),
   ],
   ignorePatterns: ["node_modules/", "dist/", "*.d.ts"],
---
+++ configs/export.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: ["sort-exports"],
+  rules: {
+    "sort-exports/sort-exports": ["error", { sortDir: "asc" }],
+  },
+};
