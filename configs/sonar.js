da31adc0f07e97e1b6d08f7223ec0f322a9303ec
Oscar Lagercrantz <oscar.lagercrantz@gmail.com>
2024-08-13 19:40:27 +0200

    Update deps

--- configs/sonar.js
+++ configs/sonar.js
@@ -1,9 +1,8 @@
 module.exports = {
-  extends: ["plugin:sonarjs/recommended"],
+  plugins: ["sonarjs"],
+  extends: ["plugin:sonarjs/recommended-legacy"],
   rules: {
     "sonarjs/no-duplicate-string": ["error", { threshold: 10 }],
-    "sonarjs/cognitive-complexity": ["error", 50],
     "sonarjs/prefer-immediate-return": "off",
-    "sonarjs/no-identical-functions": "off",
   },
 };
